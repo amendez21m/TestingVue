e91ca7e668ce4eba02d8945dcfd4f9534cb90755
anton <ama@syntelix.net>
2022-03-19 17:18:46 +0100

    first commit

---
+++ tests/unit/example.spec.js
@@ -0,0 +1,10 @@
+import {mount} from '@vue/test-utils'
+import HelloWorld from '@/components/HelloWorld.vue'
+
+describe('HelloWorld.vue', () => {
+  it('renders props.msg when passed', () => {
+    const wrapper = mount(HelloWorld)
+    expect(wrapper.isVueInstance()).toBe(true)
+
+  })
+})
